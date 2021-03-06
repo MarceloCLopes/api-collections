e2cae8bb383e63af080ac614bf925ec91dfc9908
MarceloCLopes <marc01mcl@gmail.com>
2021-03-06 14:49:28 -0300

    created test set class

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteSet.kt
@@ -0,0 +1,24 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val jonny = funcionario("Jonny", 2000.0, "CLT")
+    val pedro = funcionario("Pedro", 1500.0, "PJ")
+    val maria = funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios1 = setOf(jonny, pedro)
+    val funcionarios2 = setOf(maria)
+
+    val resultUnion =  funcionarios1.union(funcionarios2)
+    resultUnion.forEach { println(it) }
+
+    println("------------------------------")
+    val funcionarios3 = setOf(jonny, pedro, maria)
+    val resultSubtract = funcionarios3.subtract(funcionarios2)
+    resultSubtract.forEach { println(it) }
+
+    println("------------------------------")
+    val resultIntersect = funcionarios3.intersect(funcionarios2)
+    resultIntersect.forEach { println(it) }
+
+
+}
